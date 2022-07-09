e3110a2a70641490136b89105cd863b9a8a64337
Bill Cathey <billcathey@hotmail.com>
2022-07-09 15:53:48 -0400

    Add Navbar component

---
+++ components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <nav className="navbar bg-light">
+        <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+    </nav>
+  )
+}
