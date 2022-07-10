d0c5ddcb4ee4464094672c57e1fbba8975f8c567
Bill Cathey <billcathey@hotmail.com>
2022-07-10 15:42:42 -0400

    Add data file

---
+++ data.js
@@ -0,0 +1,86 @@
+export const cats = [
+    {
+        id: 1,
+        name: 'Bu Bu',
+        phone: '333-333-3333',
+        email: 'bubu@example.com',
+        image: {
+            url: 'https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+            alt: 'sleeping orange cat'
+        },
+        favored: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas. Ante in nibh mauris cursus mattis molestie a. Vel risus commodo viverra maecenas accumsan. Elit sed vulputate mi sit. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Enim praesent elementum facilisis leo vel fringilla est. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Sit amet dictum sit amet justo donec. In massa tempor nec feugiat. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Gravida neque convallis a cras. Scelerisque purus semper eget duis. Suscipit adipiscing bibendum est ultricies. Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque. Porttitor lacus luctus accumsan tortor.',
+        color: 'orange',
+        gender: 'male',
+    },
+    {
+        id: 2,
+        name: 'Sydney',
+        phone: '333-333-4444',
+        email: 'sydney@example.com',
+        image: {
+            url: 'https://images.unsplash.com/photo-1511044568932-338cba0ad803?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+            alt: 'orange Persian cat'
+        },
+        favored: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas. Ante in nibh mauris cursus mattis molestie a. Vel risus commodo viverra maecenas accumsan. Elit sed vulputate mi sit. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Enim praesent elementum facilisis leo vel fringilla est. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Sit amet dictum sit amet justo donec. In massa tempor nec feugiat. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Gravida neque convallis a cras. Scelerisque purus semper eget duis. Suscipit adipiscing bibendum est ultricies. Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque. Porttitor lacus luctus accumsan tortor.',
+        color: 'orange',
+        gender: 'male',
+    },
+    {
+        id: 3,
+        name: 'Moxie',
+        phone: '333-444-3333',
+        email: 'moxie@example.com',
+        image: {
+            url: 'https://images.unsplash.com/photo-1529257414772-1960b7bea4eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+            alt: 'gray cat'
+        },
+        favored: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas. Ante in nibh mauris cursus mattis molestie a. Vel risus commodo viverra maecenas accumsan. Elit sed vulputate mi sit. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Enim praesent elementum facilisis leo vel fringilla est. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Sit amet dictum sit amet justo donec. In massa tempor nec feugiat. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Gravida neque convallis a cras. Scelerisque purus semper eget duis. Suscipit adipiscing bibendum est ultricies. Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque. Porttitor lacus luctus accumsan tortor.',
+        color: 'gray',
+        gender: 'female',
+    },
+    {
+        id: 4,
+        name: 'Jax',
+        phone: '333-333-5555',
+        email: 'jax@example.com',
+        image: {
+            url: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+            alt: 'gray tabby cat'
+        },
+        favored: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas. Ante in nibh mauris cursus mattis molestie a. Vel risus commodo viverra maecenas accumsan. Elit sed vulputate mi sit. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Enim praesent elementum facilisis leo vel fringilla est. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Sit amet dictum sit amet justo donec. In massa tempor nec feugiat. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Gravida neque convallis a cras. Scelerisque purus semper eget duis. Suscipit adipiscing bibendum est ultricies. Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque. Porttitor lacus luctus accumsan tortor.',
+        color: 'gray',
+        gender: 'male',
+    },
+    {
+        id: 5,
+        name: 'Bessie',
+        phone: '555-333-3333',
+        email: 'bessie@example.com',
+        image: {
+            url: 'https://images.unsplash.com/photo-1511275539165-cc46b1ee89bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+            alt: 'gray cat'
+        },
+        favored: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas. Ante in nibh mauris cursus mattis molestie a. Vel risus commodo viverra maecenas accumsan. Elit sed vulputate mi sit. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Enim praesent elementum facilisis leo vel fringilla est. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Sit amet dictum sit amet justo donec. In massa tempor nec feugiat. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Gravida neque convallis a cras. Scelerisque purus semper eget duis. Suscipit adipiscing bibendum est ultricies. Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque. Porttitor lacus luctus accumsan tortor.',
+        color: 'gray',
+        gender: 'female',
+    },
+    {
+        id: 6,
+        name: 'Rosie',
+        phone: '333-333-7777',
+        email: 'rosie@example.com',
+        image: {
+            url: 'https://images.unsplash.com/photo-1472491235688-bdc81a63246e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+            alt: 'white and gray cat'
+        },
+        favored: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas. Ante in nibh mauris cursus mattis molestie a. Vel risus commodo viverra maecenas accumsan. Elit sed vulputate mi sit. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Enim praesent elementum facilisis leo vel fringilla est. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Sit amet dictum sit amet justo donec. In massa tempor nec feugiat. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Gravida neque convallis a cras. Scelerisque purus semper eget duis. Suscipit adipiscing bibendum est ultricies. Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque. Porttitor lacus luctus accumsan tortor.',
+        color: 'white',
+        gender: 'female',
+    }
+]
