cc0ae7666ae483d035007a466990d443f2348d87
Bill Cathey <billcathey@hotmail.com>
2022-07-09 22:00:23 -0400

    Create cats page, Card component

---
+++ components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './Card.module.css'
+
+export default function Card({name, phone, email, image, id}) {
+  return (
+    <div className={styles.card}>
+      <div className={styles["card-header"]}>
+          <Image src={image.url} alt={image.alt} className={styles["card-img"]} />
+      </div>
+      <div>
+          <h3>{name}</h3>
+          <h3>{phone}</h3>
+          <h3>{email}</h3>
+      </div>
+    </div>
+  )
+}
