c935c15cb1f3b94a9ef2194052105f1ca303453a
Bill Cathey <billcathey@hotmail.com>
2022-07-10 15:50:13 -0400

    Create endpoints for /cats and /cats/:id

---
+++ pages/api/cats/[id].js
@@ -0,0 +1,7 @@
+import { cats } from "../../../data";
+
+export default function handler(req, res) {
+    const {id} = req.query
+    const cat = cats.find(cat => cat.id === Number(id))
+    res.status(200).json(cat)
+  }
